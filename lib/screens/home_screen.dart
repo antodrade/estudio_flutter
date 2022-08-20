dc2c5aa99e4de3ce659b3a104ec68473c2f2bc35
prueba2@gmail.com <prueba@gmail.com>
2022-08-19 22:54:35 -0500

    commit inicial

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Componenetes en flutter'),
+          elevation: 0,
+        ),
+        body: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  leading: const Icon(Icons.access_time_outlined),
+                  title: const Text('Nombre de ruta'),
+                  onTap: () {
+                    
+                  },
+                ),
+            separatorBuilder: (_, __) => const Divider(),
+            itemCount: 100));
+  }
+}
